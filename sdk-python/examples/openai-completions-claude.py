3fc17e82ef86b4d741c082ee26e6199dcd72eee7
John Smith <john@johnjcsmith.com>
2025-03-18 20:25:35 +1030

    chore: Add anthropic and openrouter examples

---
+++ sdk-python/examples/openai-completions-claude.py
@@ -0,0 +1,29 @@
+from agentrpc import AgentRPC
+from openai import OpenAI
+import os
+
+
+def main():
+    agentrpc = AgentRPC(api_secret=os.environ.get("AGENTRPC_API_SECRET", ""))
+    openai = OpenAI(
+        api_key=os.environ.get("ANTHROPIC_API_KEY", ""),
+        base_url="https://api.anthropic.com/v1/"
+    )
+
+    tools = agentrpc.openai.completions.get_tools()
+
+    completion = openai.chat.completions.create(
+        model="claude-3-sonnet-20240229",
+        messages=[{"role": "user", "content": "What is the weather in Melbourne?"}],
+        tools=tools,
+    )
+
+    if completion.choices[0].message.tool_calls:
+        for tool_call in completion.choices[0].message.tool_calls:
+            print("Agent is calling Tool", tool_call.function.name)
+            result = agentrpc.openai.completions.execute_tool(tool_call)
+            print(result)
+
+
+if __name__ == "__main__":
+    main()
